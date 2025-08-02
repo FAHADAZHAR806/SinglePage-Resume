731547f3c1beead80285c5a0b486de88717e54a2
FADI <fahadkiyani28@gmail.com>
2025-08-03 00:36:33 +0500

    build

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -4,7 +4,7 @@ export default function Intro() {
   return (
     <>
       <div className="flex flex-col items-center justify-center m-auto gap-4 py-20 text-center px-10">
-        <h1 className="text-3xl">Let's Build Stunning Interfaces Together</h1>
+        <h1 className="text-3xl">Let,s Build Stunning Interfaces Together</h1>
         <p className="text-xl opacity-40">
           I’m a Front-End Developer crafting beautiful and functional websites
           with React, Tailwind, and modern web technologies.
